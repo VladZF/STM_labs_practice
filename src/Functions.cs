945be782d905976db8db153d2603ece0240e06ff
Vladislav <104864837+VladZF@users.noreply.github.com>
2024-06-24 21:28:49 +0300

    bubble sort and binary search moved to Functions.cs

---
+++ src/Functions.cs
@@ -0,0 +1,44 @@
+﻿namespace src;
+
+public class Functions
+{
+    public static void BubbleSort(ref int[] arr)
+    {
+        for (var i = arr.Length - 1; i >= 0; --i)
+        {
+            var flag = false;
+            for (var j = 0; j < i; ++j)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    flag = true;
+                    (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]);
+                }
+            }
+            if (!flag)
+                return;
+        }
+    }
+
+    public static int MyBinarySearch(int[] arr, int target)
+    {
+        var left = 0;
+        var right = arr.Length;
+        while (left <= right)
+        {
+            var mid = (left + right) / 2;
+            if (target < arr[mid])
+                right = mid - 1;
+            else if (target > arr[mid])
+                left = mid + 1;
+            else
+            {
+                while (mid >= 0 && arr[mid] == target)
+                    mid--;
+                return mid + 1;
+            }
+        }
+
+        return -1;
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,50 +1,13 @@
-﻿var array = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
-BubbleSort(ref array);
+﻿using src;
+
+Console.WriteLine("Insert array: ");
+var array = Console.ReadLine()!.Split().Select(int.Parse).ToArray();
+Console.WriteLine("Sorted array: ");
+Functions.BubbleSort(ref array);
 foreach (var item in array)
     Console.Write(item + " ");
-Console.WriteLine("\nInsert target for search in array");
+Console.WriteLine("\nInsert target for search in sorted array: ");
 var target = int.Parse(Console.ReadLine()!);
-var index = MyBinarySearch(array, target);
+var index = Functions.MyBinarySearch(array, target);
 Console.WriteLine(index == -1 ? "There is no element in array" : $"index of the element is {index}");
-return;
 
-void BubbleSort(ref int[] arr)
-{
-    for (var i = arr.Length - 1; i >= 0; --i)
-    {
-        var flag = false;
-        for (var j = 0; j < i; ++j)
-        {
-            if (arr[j] > arr[j + 1])
-            {
-                flag = true;
-                (arr[j], arr[j + 1]) = (arr[j + 1], arr[j]);
-            }
-
-            if (!flag)
-                return;
-        }
-    }
-}
-
-int MyBinarySearch(int[] arr, int target)
-{
-    var left = 0;
-    var right = arr.Length;
-    while (left <= right)
-    {
-        var mid = (left + right) / 2;
-        if (target < arr[mid])
-            right = mid - 1;
-        else if (target > arr[mid])
-            left = mid + 1;
-        else
-        {
-            while (mid >= 0 && arr[mid] == target)
-                mid--;
-            return mid + 1;
-        }
-    }
-
-    return -1;
-}
