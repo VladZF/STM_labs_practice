2dcdf840998249da52372e346fa825cec67fd7d3
Vladislav <104864837+VladZF@users.noreply.github.com>
2024-06-30 15:14:15 +0300

    handling of input format and src folder

--- src/SampleGit/Program.cs
+++ src/SampleGit/Program.cs
@@ -14,6 +14,16 @@ try
 }
 catch (FormatException e)
 {
+    Console.WriteLine("Error: wrong format of array input");
+    return;
+}
+catch (OverflowException e)
+{
+    Console.WriteLine("Error: detected number(s) greater than int32");
+    return;
+}
+catch (Exception e)
+{
     Console.WriteLine($"Error: {e.Message}");
     return;
 }
@@ -23,7 +33,27 @@ Functions.BubbleSort(ref array);
 foreach (var item in array)
     Console.Write(item + " ");
 Console.WriteLine("\nInsert target for search in sorted array: ");
-var target = int.Parse(Console.ReadLine()!);
+int target;
+try
+{
+    target = int.Parse(Console.ReadLine()!);
+}
+catch (FormatException e)
+{
+    Console.WriteLine("Error: wrong format of target input");
+    return;
+}
+catch (OverflowException e)
+{
+    Console.WriteLine("Error: target is greater than int32");
+    return;
+}
+catch (Exception e)
+{
+    Console.WriteLine($"Error: {e.Message}");
+    return;
+}
+
 var index = Functions.MyBinarySearch(array, target);
 Console.WriteLine(index == -1 ? "There is no element in array" : $"index of the element is {index}");
 
