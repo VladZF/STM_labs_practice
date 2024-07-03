a8fd869b0d39e76eec54a3e75be58f4cad27e3fc
Vladislav <104864837+VladZF@users.noreply.github.com>
2024-07-03 13:03:44 +0300

    Task 3 completed

--- src/BaseAlgorithms/Program.cs
+++ src/BaseAlgorithms/Program.cs
@@ -2,8 +2,39 @@
 
 class Program
 {
+    static void CompressBinarySequenceConsole()
+    {
+        Console.Write("Insert sequence: ");
+        try
+        {
+            var sequence = Console.ReadLine()!;
+            var compressed = Solution.CompressBinarySequence(sequence);
+            Console.WriteLine($"Compressed sequence: {compressed}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Write("1 - Task 1\n" +
+                      "2 - Task 2\n" +
+                      "3 - Task 3\n" +
+                      "4 - Task 4\n" +
+                      "5 - Task 5\n" +
+                      "6 - Task 6\n" +
+                      "Insert operation: ");
+        var operation = int.Parse(Console.ReadLine() ?? string.Empty);
+        switch (operation)
+        {
+            case 3:
+                CompressBinarySequenceConsole();
+                break;
+            default:
+                Console.WriteLine("Incorrect operation");
+                break;
+        }
     }
 }
--- src/BaseAlgorithms/Solution.cs
+++ src/BaseAlgorithms/Solution.cs
@@ -1,6 +1,33 @@
-﻿namespace BaseAlgorithms;
+﻿using System.Text;
+
+namespace BaseAlgorithms;
 
 public static class Solution
 {
-    
+    public static string CompressBinarySequence(string sequence)
+    {
+        if (sequence.Length == 0)
+            return string.Empty;
+        var answer = new StringBuilder();
+        var counter = 0;
+        if (sequence[^1] == '0')
+            throw new ArgumentException("Impossible to compress sequence");
+        foreach (var c in sequence)
+        {
+            if (c != '0' && c != '1')
+            {
+                throw new FormatException("Sequence is not binary");
+            }
+            if (c == '1')
+            {
+                answer.Append((char)('a' + counter));
+                counter = -1;
+            }
+
+            counter++;
+            if (counter > 26)
+                throw new ArgumentException("Impossible to compress sequence");
+        }
+        return answer.ToString();
+    }
 }
