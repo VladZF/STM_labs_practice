6265d3f46f8f4d5b52c8c22f3c40827da747a820
Vladislav <104864837+VladZF@users.noreply.github.com>
2024-07-03 14:06:10 +0300

    Task 2 completed

--- src/BaseAlgorithms/Program.cs
+++ src/BaseAlgorithms/Program.cs
@@ -1,9 +1,53 @@
-﻿namespace BaseAlgorithms;
+﻿using System.Globalization;
+
+namespace BaseAlgorithms;
 
 class Program
 {
+    static void WallAndStickConsole()
+    {
+        var englishCulture = CultureInfo.GetCultureInfo("en-US");
+        double length = 0, distance = 0, step = 0;
+        try
+        {
+            Console.Write("Insert length of stick: ");
+            length = double.Parse(Console.ReadLine()!, englishCulture);
+            Console.Write("Insert distance between wall and bottom end of stick: ");
+            distance = double.Parse(Console.ReadLine()!, englishCulture);
+            Console.Write("Insert step of distance's change: ");
+            step = double.Parse(Console.ReadLine()!, englishCulture);
+            var answerList = Solution.WallAndStick(length, distance, step);
+            foreach (var angle in answerList)
+            {
+                Console.WriteLine($"{double.Round(angle, 2)} ");
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        
+    }
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Write("1 - Task 1\n" +
+                      "2 - Task 2\n" +
+                      "3 - Task 3\n" +
+                      "4 - Task 4\n" +
+                      "5 - Task 5\n" +
+                      "6 - Task 6\n" +
+                      "Insert operation: ");
+        var operation = int.Parse(Console.ReadLine() ?? string.Empty);
+        switch (operation)
+        {
+            case 2:
+                WallAndStickConsole();
+                break;
+            default:
+                Console.WriteLine("Incorrect operation");
+                break;
+        }
     }
 }
--- src/BaseAlgorithms/Solution.cs
+++ src/BaseAlgorithms/Solution.cs
@@ -2,5 +2,22 @@
 
 public static class Solution
 {
-    
+    public static List<double> WallAndStick(double length, double distance, double step)
+    {
+        List<double> answer = [];
+        if (step > 3)
+        {
+            throw new ArgumentException("Step is greater than 3");
+        }
+        if (distance >= length)
+        {
+            return [0];
+        }
+        for (var h = distance; h >= 0 && Math.Abs(h - distance) <= 3 && h < length; h += step)
+        {
+            answer.Add(Math.Acos(h / length) * 180 / Math.PI);
+        }
+
+        return answer;
+    }
 }
