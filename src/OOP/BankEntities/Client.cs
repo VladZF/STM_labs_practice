3b8d86047eb438216e16cc275d3791a065eb5999
Vladislav <104864837+VladZF@users.noreply.github.com>
2024-07-25 00:33:41 +0300

    Client class

---
+++ src/OOP/BankEntities/Client.cs
@@ -0,0 +1,81 @@
+﻿namespace OOP;
+
+public class Client
+{
+    private string _name;
+    private string _surname;
+    private string _patronymic;
+    private Phone? _phone;
+    private Passport? _passport;
+    
+    public Client(string name, string surname, string patronymic, string? phone = null, string? passport = null)
+    {
+        Name = name;
+        Surname = surname;
+        Patronymic = patronymic;
+        Phone = phone;
+        Passport = passport;
+    }
+    
+    public string Name
+    {
+        get => _name;
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("Empty name");
+            }
+
+            _name = value;
+        }
+    }
+    
+    public string Surname
+    {
+        get => _surname;
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("Empty surname");
+            }
+
+            _surname = value;
+        }
+    }
+    
+    public string Patronymic
+    {
+        get => _patronymic;
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("Empty patronymic");
+            }
+
+            _patronymic = value;
+        }
+    }
+    
+    public string? Phone
+    {
+        get => _phone?.Value ?? "No phone";
+        set
+        {
+            if (value != null) _phone = OOP.Phone.From(value);
+            _phone = null;
+        }
+    }
+
+    public string? Passport
+    {
+        get => _passport?.Value ?? "No passport";
+        set
+        {
+            if (value != null) _passport = OOP.Passport.From(value);
+            _passport = null;
+        }
+    }
+}
---
+++ src/OOP/ValueObjects/Passport.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+using ValueOf;
+
+namespace OOP;
+
+public partial class Passport : ValueOf<string, Passport>
+{
+    protected override void Validate()
+    {
+        if (!PassportPattern().IsMatch(Value))
+        {
+            throw new FormatException("Passport number has incorrect format");
+        }
+    }
+    
+    [GeneratedRegex(@"^(\+7|8)?[\s\-]?\(?[489][0-9]{2}\)?[\s\-]?[0-9]{3}[\s\-]?[0-9]{2}[\s\-]?[0-9]{2}$")]
+    private static partial Regex PassportPattern();
+}
