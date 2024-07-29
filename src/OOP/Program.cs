5257ca6bf6554e66751bcac8954b9f240769ee4f
Vladislav <104864837+VladZF@users.noreply.github.com>
2024-07-29 22:23:39 +0300

    Handling of file errors

--- src/OOP/Program.cs
+++ src/OOP/Program.cs
@@ -6,9 +6,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Insert path to file with clients info:");
-        var filepath = Console.ReadLine()!;
-        var bank = new BankApplication(new DataBase(filepath));
-        bank.Start();
+        try
+        {
+            Console.WriteLine("Insert path to file with clients info:");
+            var filepath = Console.ReadLine()!;
+            if (!filepath.EndsWith(".json"))
+            {
+                Console.WriteLine("Error: File must be .json");
+                return;
+            }
+            var bank = new BankApplication(new DataBase(filepath));
+            bank.Start();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error in DataBase connection to file.\nError message: {e.Message}");
+        }
     }
 }
