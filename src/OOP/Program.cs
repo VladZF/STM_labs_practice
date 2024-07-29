78b307b8139be675fae34bba30564630c8a54838
Vladislav <104864837+VladZF@users.noreply.github.com>
2024-07-29 20:41:33 +0300

    Consultant class test

--- src/OOP/Program.cs
+++ src/OOP/Program.cs
@@ -1,9 +1,27 @@
-﻿namespace OOP;
+﻿using OOP.Enums;
+
+namespace OOP;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var path = args[0];
+        var db = new DataBase(path);
+        var consultant = new Consultant();
+        var id = db.Clients.First(client => client.Name == "Alexey").Id;
+        Console.WriteLine(consultant.GetClientProperty(db, id, ClientProperty.Name));
+        Console.WriteLine(consultant.GetClientProperty(db, id, ClientProperty.Surname));
+        Console.WriteLine(consultant.GetClientProperty(db, id, ClientProperty.Patronymic));
+        Console.WriteLine(consultant.GetClientProperty(db, id, ClientProperty.Passport));
+        Console.WriteLine(consultant.GetClientProperty(db, id, ClientProperty.Phone));
+        try 
+        {
+            consultant.ChangeClientProperty(db, id, ClientProperty.Phone, "+79822315521");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
     }
 }
