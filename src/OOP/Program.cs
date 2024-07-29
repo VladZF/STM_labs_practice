55914a09eab8a985510a4e0521289028133b0f92
Vladislav <104864837+VladZF@users.noreply.github.com>
2024-07-29 22:11:09 +0300

    CLI start added

--- src/OOP/Program.cs
+++ src/OOP/Program.cs
@@ -6,22 +6,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        var path = args[0];
-        var db = new DataBase(path);
-        var consultant = new Consultant();
-        var id = db.Clients.First(client => client.Name == "Alexey").Id;
-        Console.WriteLine(consultant.GetClientProperty(db, id, ClientProperty.Name));
-        Console.WriteLine(consultant.GetClientProperty(db, id, ClientProperty.Surname));
-        Console.WriteLine(consultant.GetClientProperty(db, id, ClientProperty.Patronymic));
-        Console.WriteLine(consultant.GetClientProperty(db, id, ClientProperty.Passport));
-        Console.WriteLine(consultant.GetClientProperty(db, id, ClientProperty.Phone));
-        try 
-        {
-            consultant.ChangeClientProperty(db, id, ClientProperty.Phone, "+79822315521");
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e.Message);
-        }
+        Console.WriteLine("Insert path to file with clients info:");
+        var filepath = Console.ReadLine()!;
+        var bank = new BankApplication(new DataBase(filepath));
+        bank.Start();
     }
 }
