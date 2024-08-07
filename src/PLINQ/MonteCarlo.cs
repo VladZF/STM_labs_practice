ecfc57c963ac44e37185c09ef126b4f9ba33f1b1
Vladislav <104864837+VladZF@users.noreply.github.com>
2024-08-08 00:44:20 +0300

    PLINQ task completed

---
+++ src/PLINQ/MonteCarlo.cs
@@ -0,0 +1,39 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace PLINQ;
+
+public class MonteCarlo
+{
+    private const int CircleRadius = 1;
+    private const int PointsCount = 1000000;
+
+    private readonly List<(double x, double y)> _pointsList = [];
+    private static bool InCircle(double x, double y) => x * x + y * y <= CircleRadius * CircleRadius;
+    
+    [GlobalSetup]
+    public void Setup()
+    {
+        var random = new Random();
+        for (var i = 0; i < PointsCount; i++)
+        {
+            _pointsList.Add((random.NextDouble(), random.NextDouble()));
+        }
+    }
+    
+    [Benchmark]
+    public double GetPi()
+    {
+        var pointsInCircle = _pointsList
+            .Count(point => InCircle(point.x, point.y));
+        return 4 * ((double)pointsInCircle / PointsCount);
+    }
+    
+    [Benchmark]
+    public double GetPiParallel()
+    {
+        var pointsInCircle = _pointsList
+            .AsParallel()
+            .Count(point => InCircle(point.x, point.y));
+        return 4 * ((double)pointsInCircle / PointsCount);
+    }
+}
