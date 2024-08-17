0310d2aed93a7ead7b4bf963a540a77ef6252142
Vladislav <104864837+VladZF@users.noreply.github.com>
2024-08-17 17:59:16 +0300

    Added handlers for controls

--- src/ThreadsUI/MainWindow.xaml.cs
+++ src/ThreadsUI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Reflection;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace ThreadsUI;
@@ -9,40 +10,35 @@ namespace ThreadsUI;
 public partial class MainWindow : Window
 {
     private int _launchedThreadsCount;
+    private int _threadsForStartCount;
+    private int _delayForStart;
+    private int _itemsPerIterationForStart;
     private const int MaxThreads = 16;
 
     public MainWindow()
     {
         InitializeComponent();
         _launchedThreadsCount = 0;
+        StopButton.IsEnabled = false;
+        StopAllButton.IsEnabled = false;
     }
 
     private void StartButton_Click(object sender, RoutedEventArgs e)
     {
-        Button button = (Button)sender;
-        if (ThreadCount.Text == "")
+        if (_threadsForStartCount == 0)
         {
-            MessageBox.Show("Введите количество запускаемых потоков");
+            MessageBox.Show("Потоков не было запущено");
+        }
+
+        if (_launchedThreadsCount + _threadsForStartCount > MaxThreads)
+        {
+            MessageBox.Show($"Количество запущенных потоков должно быть не больше {MaxThreads}");
             return;
         }
-        var threadForStartCount = int.Parse(ThreadCount.Text);
-        if (threadForStartCount < 0)
-        {
-            MessageBox.Show("Количество запускаемых потоков должно быть положительным числом");
-            return;
-        }
-        if (threadForStartCount == 0)
-        {
-            MessageBox.Show("Потоков не запущено");
-            return;
-        }
-        if (_launchedThreadsCount + threadForStartCount > MaxThreads)
-        {
-            MessageBox.Show($"количество работающих потоков не должно превышать {MaxThreads}");
-            return;
-        }
-        _launchedThreadsCount += threadForStartCount;
+        _launchedThreadsCount += _threadsForStartCount;
         WorkingThreadsInfoBlock.Text = $"Количество запущенных потоков: {_launchedThreadsCount}";
+        StopButton.IsEnabled = true;
+        StopAllButton.IsEnabled = true;
     }
 
     private void StopButton_Click(object sender, RoutedEventArgs e)
@@ -55,6 +51,8 @@ public partial class MainWindow : Window
         {
             MessageBox.Show("Все потоки остановлены");
             WorkingThreadsInfoBlock.Text = "Ни один поток не запущен";
+            StopButton.IsEnabled = false;
+            StopAllButton.IsEnabled = false;
             return;
         }
         WorkingThreadsInfoBlock.Text = $"Количество запущенных потоков: {_launchedThreadsCount}";
@@ -63,10 +61,90 @@ public partial class MainWindow : Window
     private void StopAllButton_Click(object sender, RoutedEventArgs e)
     {
         _launchedThreadsCount = 0;
+        MessageBox.Show("Все потоки остановлены");
         WorkingThreadsInfoBlock.Text = "Ни один поток не запущен";
+        StopButton.IsEnabled = false;
+        StopAllButton.IsEnabled = false;
     }
 
     private void ThreadCount_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        var textBox = (TextBox)sender;
+        if (textBox.Text == "")
+        {
+            _launchedThreadsCount = 0;
+            return;
+        }
+        try
+        {
+            _threadsForStartCount = int.Parse(textBox.Text);
+        }
+        catch (FormatException)
+        {
+            MessageBox.Show("Вы ввели символ отличный от цифры");
+            textBox.Clear();
+        }
+        catch (Exception)
+        {
+            MessageBox.Show("Произошла ошибка");
+            textBox.Clear();
+        }
+    }
+
+    private void Delay_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        var textBox = (TextBox)sender;
+        if (textBox.Text == "")
+        {
+            _launchedThreadsCount = 0;
+            return;
+        }
+        try
+        {
+            _delayForStart = int.Parse(textBox.Text);
+        }
+        catch (FormatException)
+        {
+            MessageBox.Show("Вы ввели символ отличный от цифры");
+            textBox.Clear();
+        }
+        catch (Exception)
+        {
+            MessageBox.Show("Произошла ошибка");
+            textBox.Clear();
+        }
+    }
+
+    private void ItemsPerIteration_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        var textBox = (TextBox)sender;
+        if (textBox.Text == "")
+        {
+            _launchedThreadsCount = 0;
+            return;
+        }
+        try
+        {
+            _itemsPerIterationForStart = int.Parse(textBox.Text);
+        }
+        catch (FormatException)
+        {
+            MessageBox.Show("Вы ввели символ отличный от цифры");
+            textBox.Clear();
+        }
+        catch (Exception)
+        {
+            MessageBox.Show("Произошла ошибка");
+            textBox.Clear();
+        }
+    }
+
+    private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+
+    }
+
+    private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
     {
 
     }
